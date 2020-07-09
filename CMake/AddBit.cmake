b3d0ef8d128975b29e65c500a7ea672ba34dd545
Vitalii Minnakhmetov <restlessmonkey@ya.ru>
2020-07-09 13:25:04 +0300

    add CMake add_bit utility function for downstream convenience

---
+++ CMake/AddBit.cmake
@@ -0,0 +1,17 @@
+function (add_bit)
+  set(options "")
+  set(oneValueArgs TARGET BITS_DIR LIBNAME_PREFIX)
+  set(multiValueArgs BITS)
+  cmake_parse_arguments(ADD_BIT "${options}" "${oneValueArgs}"
+    "${multiValueArgs}" ${ARGN} )
+
+  foreach (BIT ${ADD_BIT_BITS})
+    add_subdirectory(${ADD_BIT_BITS_DIR}/${BIT} ${BIT})
+    set(LIB_NAME ${ADD_BIT_LIBNAME_PREFIX}-${BIT})
+    target_link_libraries(${ADD_BIT_TARGET} ${LIB_NAME})
+    get_property(T TARGET ${LIB_NAME} PROPERTY TYPE)
+    if (NOT ${T} STREQUAL INTERFACE_LIBRARY)
+      target_compile_options(${LIB_NAME} PRIVATE ${ADDITIONAL_CXX_FLAGS})
+    endif ()
+  endforeach ()
+endfunction ()
